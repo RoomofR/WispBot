02358454aba1c84dc474a47de3387ca5a2e96c90
Unknown <alawajiryan@gmail.com>
2017-12-03 19:22:38 +0200

    New command template
    File name changed to _template.js

---
+++ commands/_template.js
@@ -0,0 +1,13 @@
+module.exports = {
+	enabled: false,
+	name: "template",
+	aliases: [],
+	users: [],
+	description: "TODO",
+	usage: "TODO",
+	run: run
+}
+
+function run(client,message,args){
+	console.log("TEST",message);
+}
