df2c64f3a4b1228185dda7b3d38d33d22caa2d37
RoomofR <alawajiryan@gmail.com>
2017-12-05 00:13:53 +0200

    Added Voicechannel event logging

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,5 +1,26 @@
 exports.run = async (client,oldUser,newUser) => {
-	let oldVC = oldUser.voiceChannel ? oldUser.voiceChannel.name : "None";
-	let newVC = newUser.voiceChannel ? newUser.voiceChannel.name : "None";
-	console.log(`${oldVC} --> ${newVC}`);
+/*	console.log(`${oldUser.user.username} :: ${oldUser.voiceChannel.name ? oldUser.voiceChannel.name : "None"} --> ${newUser.voiceChannel.name ? newUser.voiceChannel.name : "None"}`);
+*/
+	
+	let embed = {
+		color: 7419784,
+		author: {
+			name:`${newUser.user.username}${newUser.nickname==null ? "" : ` (${newUser.nickname})`}`,
+			icon_url:newUser.user.avatarURL
+		}
+	}
+
+	//Moved to another channel
+	if(typeof oldUser.voiceChannel != 'undefined' && typeof newUser.voiceChannel != 'undefined'){
+		if(oldUser.voiceChannel==newUser.voiceChannel) return;
+		embed.description = `MOVED [${oldUser.voiceChannel.name}] ==> [${newUser.voiceChannel.name}]`;
+	}
+	//Joined Channels
+	else if(typeof oldUser.voiceChannel == 'undefined') embed.description = `JOINED ==> [${newUser.voiceChannel.name}]`;
+	//Left Channels
+	else embed.description = `[${oldUser.voiceChannel.name}] ==> LEFT`;
+
+	newUser.guild.channels.find("name","wisp_bot")
+			 .send({embed:embed});
+	console.log(`${embed.author.name} :: ${embed.description}`);
 }
